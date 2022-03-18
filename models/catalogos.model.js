a66efe1fcd5f1ba3ee76426ef8954688d5c7822a
ravila2020 <59663271+ravila2020@users.noreply.github.com>
2022-03-18 17:40:22 -0600

    Homologacion RA

--- models/catalogos.model.js
+++ models/catalogos.model.js
@@ -4,4 +4,46 @@ const DictamenesModel = function(datDictamenes) {
     this.Estatus = datDictamenes.Estatus;
 };
 
-module.exports = { DictamenesModel };
+const PerfilesModel = function(datPerfiles) {
+    this.IdPerfil = datPerfiles.IdPerfil;
+    this.Descripcion = datPerfiles.Descripcion;
+    this.FechaRegistro = datPerfiles.FechaRegistro;
+    this.Estatus = datPerfiles.Estatus;
+};
+
+const EntidadesModel = function(datEntidades) {
+    this.IdEntidadFederal = datEntidades.IdEntidadFederal;
+    this.Nombre = datEntidades.Nombre;
+    this.Abreviacion = datEntidades.Abreviacion;
+};
+
+const HGasModel = function(datHistoricoGas) {
+    this.IdHistoricoGas = datHistoricoGas.IdHistoricoGas;	
+    this.FechaAlta = datHistoricoGas.FechaAlta;			 
+    this.FechaDesde = datHistoricoGas.FechaDesde; 		
+    this.FechaHasta = datHistoricoGas.FechaHasta;		 
+    this.IdEntidadFederal =	datHistoricoGas.IdEntidadFederal; 
+    this.IdMunicipio = datHistoricoGas.IdMunicipio; 		
+    this.PrecioKg = datHistoricoGas.PrecioKg; 			
+    this.PrecioLtr = datHistoricoGas.PrecioLtr;
+    this.NombreE = datHistoricoGas.NombreE; 
+    this.NombreM = datHistoricoGas.NombreM; 
+};
+
+const HGasolinaModel = function(datHistoricoGasolina) {
+    this.IdHistoricoGasolina = datHistoricoGasolina.IdHistoricoGasolina;	
+    this.FechaAlta = datHistoricoGasolina.FechaAlta;			 
+    this.FechaDesde = datHistoricoGasolina.FechaDesde; 		
+    this.FechaHasta = datHistoricoGasolina.FechaHasta;		 
+    this.IdEntidadFederal =	datHistoricoGasolina.IdEntidadFederal; 
+    this.IdMunicipio = datHistoricoGasolina.IdMunicipio; 		
+    this.PrecioLtr = datHistoricoGasolina.PrecioLtr;
+    this.NombreE = datHistoricoGasolina.NombreE; 
+    this.NombreM = datHistoricoGasolina.NombreM; 
+};
+
+module.exports = { DictamenesModel,
+                  PerfilesModel,
+                  EntidadesModel,
+                  HGasModel, 
+                  HGasolinaModel };
