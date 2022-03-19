76e99c2f684fbfb93498546ae0e25aac0d2e1a11
ravila2020 <59663271+ravila2020@users.noreply.github.com>
2022-03-18 18:54:01 -0600

    Homologacion RA

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -19,4 +19,20 @@ const UsuarioTokenModel = function(datUsuario) {
     this.Perfil = datUsuario.Perfil;
 };
 
-module.exports = { UsuarioAccesoModel, UsuarioModel, UsuarioTokenModel };
+const UsuarioFullModel = function(datUsuario) {
+    this.IdUsuario           = datUsuario.IdUsuario;        
+    this.Nombre              = datUsuario.Nombre;           
+    this.Contrasenia         = datUsuario.Contrasenia;      
+    this.IdEmpleado          = datUsuario.IdEmpleado;       
+    this.IdPerfil            = datUsuario.IdPerfil;         
+    this.Perfil              = datUsuario.Perfil;           
+    this.FechaRegistro       = datUsuario.FechaRegistro;    
+    this.Estatus             = datUsuario.Estatus;          
+    this.email               = datUsuario.email;            
+    this.Bloqueado           = datUsuario.Bloqueado;        
+    this.Intentos            = datUsuario.Intentos;         
+    this.UltimaTransaccion   = datUsuario.UltimaTransaccion;
+    
+};
+module.exports = { UsuarioFullModel,
+    UsuarioAccesoModel, UsuarioModel, UsuarioTokenModel };
